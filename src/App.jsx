e41eaf1c79aa685928e8b5d69f5ecd19c854d2ea
phatemeh amiri <fatemehamiri978@gmail.com>
2025-09-12 22:05:03 +0330

    complete page layout ?

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Outlet } from 'react-router-dom';
 import './App.css'
 import Sidebar from './component/sidebar/sidebar'
 
@@ -8,10 +8,17 @@ function App() {
 
   return (
     <>
-    <BrowserRouter>
-    <Sidebar/>
-    </BrowserRouter>
-     
+
+      <div className='w-[100%]  h-[50rem] bg-sky-900 h-[100%]  flex p-[1rem] m-0'>
+        <BrowserRouter>
+          <Sidebar />
+          <div  className='w-[85%]  h-[100%] rounded-3xl  bg-red-100'>
+            <Outlet/>
+          </div>
+        </BrowserRouter>
+      </div>
+
+
     </>
   )
 }
--- src/component/sidebar/sidebar.jsx
+++ src/component/sidebar/sidebar.jsx
@@ -3,9 +3,9 @@ import { Link } from "react-router-dom";
 function Sidebar() {
    return(
     <>
-    <div  className="bg-sky-900 h-[100%]   w-[20%]">
-         <Link  to="/"> <button>Today Task</button></Link>
-         <Link  to="/calender"><button>Calender</button></Link>
+    <div  className="bg-sky-900 h-[100%]   w-[20%]   flex flex-col   text-xl  text-white  gap-4  p-3 ">
+         <Link  to="/"> <button   className="hover:bg-sky-700   w-[100%]  p-2  rounded-3xl">Today Task</button></Link>
+         <Link  to="/calender"><button    className="hover:bg-sky-700   w-[100%]  p-2  rounded-3xl">Calender</button></Link>
     </div>
     </>
    )
