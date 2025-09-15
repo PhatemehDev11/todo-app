d358777909700512a26fa62f23edb65f3ef87fd5
phatemeh amiri <fatemehamiri978@gmail.com>
2025-09-15 22:57:53 +0330

    responsive content: TaskList, TaskForm &...

--- src/component/addTaskForm/addTaskForm.jsx
+++ src/component/addTaskForm/addTaskForm.jsx
@@ -1,29 +1,31 @@
 
 import BaseButton from "../common/BaseButton";
 function AddTaskForm() {
-  return(
-    <>
-     <form
-        className="flex flex-col gap-3  bg-sky-100 rounded-lg shadow-md p-4  mt-6  w-[23%]"
-      >
-        <input
-          type="text"
-          placeholder="Task Name"
-     
+    return (
+        <>
+        <div  className="bg-sky-100 rounded-lg shadow-md p-2 m-7">
+        <form
+                className="flex flex-col gap-3 p-4   "
+            >
+                   <h2 class="text-xl  font-bold mb-4   text-sky-900">Add Task</h2>
+                <input
+                    type="text"
+                    placeholder="Task Name"
+                    className="p-2 rounded border border-gray-300  "
+                />
+                <select
+                    className="p-2 rounded border border-gray-300"
+                >
+                    <option value="not-started">Not Started</option>
+                    <option value="in-progress">In Progress</option>
+                    <option value="completed">Completed</option>
+                </select>
+                <BaseButton Text="Add" />
+            </form>
+        </div>
           
-          className="p-2 rounded border border-gray-300"
-        />
-        <select
-          className="p-2 rounded border border-gray-300"
-        >
-          <option value="not-started">Not Started</option>
-          <option value="in-progress">In Progress</option>
-          <option value="completed">Completed</option>
-        </select>
-       <BaseButton   Text="Add"  />
-      </form>
-    </>
-  )
+        </>
+    )
 }
 
 export default AddTaskForm;
