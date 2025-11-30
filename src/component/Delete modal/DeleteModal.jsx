40157df2f0775ce5fcdaa9f16df3e12fcb9539e3
phatemeh amiri <fatemehamiri978@gmail.com>
2025-11-30 14:56:30 +0330

    add delete logic in for delete modal

--- src/component/Delete modal/DeleteModal.jsx
+++ src/component/Delete modal/DeleteModal.jsx
@@ -2,22 +2,32 @@ import BaseButton from "../common/BaseButton";
 
 
 
-function DeleteModal(){
-    return(
+function DeleteModal({task , onConfirm, onClose}) {
+    return (
         <>
-         <div className="fixed inset-0 bg-black bg-opacity-70  transition-opacity
+            <div className="fixed inset-0 bg-black bg-opacity-70  transition-opacity
            duration-300  flex items-center justify-center  ">
-                <div className="w-[20rem]  h-[18rem]  bg-sky-200  rounded-2xl p-3">
-                    <h2 className="text-xl font-bold text-sky-900 mb-4">Are you sure you want to delete?</h2>
-                        <button  className="bg-sky-600 hover:bg-sky-700 text-white p-2 rounded "
-                        type="submit"
+                <div className="w-[20rem]  h-[10rem]  bg-sky-200  rounded-2xl p-3">
+                    <h2 className="text-xl font-bold text-sky-900 mb-4">Are you sure you want to delete"{task.title}"?</h2>
+                    <div  className="w-[100%] flex items-center  justify-between ">
+                        <button 
+                            className="bg-sky-600 hover:bg-sky-700 text-white p-2 rounded w-[9rem]"
+                            type="submit"
+                            onClick={() => {
+                                onConfirm(task.id);
+                                onClose;
+                            }}
                         >
                             Delete
+                        </button  >
+                        <button 
+                         className="bg-sky-600 hover:bg-sky-700 text-white p-2 rounded w-[9rem] "
+                         onClick={onClose}
+                        >
+                           cancle
                         </button>
-                        <BaseButton Text="cancle" 
-                       
-                        />
-        
+                        
+                    </div>
                 </div>
             </div>
         </>
--- src/page/mainPage.jsx
+++ src/page/mainPage.jsx
@@ -4,6 +4,7 @@ import TaskList from "../component/TaskList/TaskList";
 import { useState, useEffect } from "react";
 import TaskData from "../data/Tasks.json"
 import EditModal from "../component/updateTaskForm/UpdateTask";
+import DeleteModal from "../component/Delete modal/deleteModal";
 
 function MainPage() {
 
@@ -28,18 +29,30 @@ function MainPage() {
   useEffect(() => {
     localStorage.setItem("tasks", JSON.stringify(tasks));
   }, [tasks]);
+
+
+
+  const [deletingTask, setDeletingTask] = useState(null)
   return (
     <>
       <div className="w-full">
         <SearchBar Search={Search} setSearch={setSearch} />
         <div className="w-full  h-auto flex gap-5 flex  p-7 mt-10  md:mt-2 md:flex-row  flex-col-reverse  ">
-          <TaskList tasks={filteredTasks} setTasks={setTasks}   setEditingTask={setEditingTask}  />
+          <TaskList tasks={filteredTasks} setTasks={setTasks} setEditingTask={setEditingTask} setDeletingTask={setDeletingTask} />
           <AddTaskForm tasks={tasks} setTasks={setTasks} />
           {editingTask && (
-            <EditModal task={editingTask} 
-            setTasks={setTasks}
-            onClose={() => setEditingTask(null)} />
+            <EditModal task={editingTask}
+              setTasks={setTasks}
+              onClose={() => setEditingTask(null)} />
           )}
+          {deletingTask && (
+            <DeleteModal
+              task={deletingTask}
+              onConfirm={(id) => setTasks(prev => prev.filter(t => t.id !== id))}
+              onClose={() => setDeletingTask(null)}
+            />
+          )}
+
 
         </div>
       </div>
