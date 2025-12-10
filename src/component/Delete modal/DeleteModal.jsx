e0265b50ce88fb74a12262d941e6b739f21b99a4
phatemeh amiri <fatemehamiri978@gmail.com>
2025-12-10 19:40:52 +0330

    edit some bug for toast

--- src/component/Delete modal/DeleteModal.jsx
+++ src/component/Delete modal/DeleteModal.jsx
@@ -6,8 +6,8 @@ function DeleteModal({task , onConfirm, onClose}) {
     return (
         <>
             <div className="fixed inset-0 bg-black bg-opacity-70  transition-opacity
-           duration-300  flex items-center justify-center  ">
-                <div className="w-[20rem]  h-[10rem]  bg-sky-200  rounded-2xl p-3">
+           duration-300  flex items-center justify-center animate-fadeIn ">
+                <div className="w-[20rem]  h-[10rem]  bg-sky-200  rounded-2xl p-3    animate-scaleIn">
                     <h2 className="text-xl font-bold text-sky-900 mb-4">Are you sure you want to delete"{task.title}"?</h2>
                     <div  className="w-[100%] flex items-center  justify-between ">
                         <button 
@@ -22,7 +22,7 @@ function DeleteModal({task , onConfirm, onClose}) {
                             Delete
                         </button  >
                         <button 
-                         className="bg-sky-600 hover:bg-sky-700 text-white p-2 rounded w-[9rem] "
+                         className="bg-red-700 hover:bg-sky-700 text-white p-2 rounded w-[9rem] "
                          onClick={onClose}
                         >
                            cancle
--- src/component/updateTaskForm/UpdateTask.jsx
+++ src/component/updateTaskForm/UpdateTask.jsx
@@ -1,62 +1,68 @@
-import { useState } from "react";
-import BaseButton from "../common/BaseButton";
+import { useState, useEffect } from "react";
 import toast from "react-hot-toast";
 
-function EditModal({ task, setTasks , onClose }) {
+function EditModal({ task, setTasks, onClose }) {
+  
+  const [title, setTitle] = useState(task ? task.title : "");
+  const [status, setStatus] = useState(task ? task.status : "Low");
 
-    const [title, setTitle] = useState(task.title || "");
-    const [status, setStatus] = useState(task.status || "Low")
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    setTasks(prev =>
+      prev.map(t => 
+        t.id === task.id ? { ...t, title, status } : t
+      )
+    );
+
+    toast.success("Task updated!");
+    onClose();
+  };
+
+  return (
+    <div className="fixed inset-0 bg-black bg-opacity-70 flex items-center justify-center  animate-fadeIn">
+      <div className="bg-sky-200 p-4 rounded-xl w-[20rem]  animate-scaleIn">
+        <h2 className="text-xl font-bold mb-4 text-sky-900">Edit Task</h2>
+
+        <form onSubmit={handleSubmit} className="flex flex-col gap-3">
+
+          <input
+            type="text"
+            className="border p-2 rounded"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+
+          <select
+            className="border p-2 rounded"
+            value={status}
+            onChange={(e) => setStatus(e.target.value)}
+          >
+            <option value="Low">Low</option>
+            <option value="Medium">Medium</option>
+            <option value="High">High</option>
+          </select>
 
 
-    const handleSubmite = (e) => {
-        e.preventDefault();
-        setTasks(prev => prev.map(t =>
-            t.id === task.id ? { ...t, title, status } : t
-        ));
-        toast.success("Task updated!");
-          onClose();
-    }
-    return (
-        <>
-            <div className="fixed inset-0 bg-black bg-opacity-70  transition-opacity
-           duration-300  flex items-center justify-center  ">
-                <div className="w-[20rem]  h-[18rem]  bg-sky-200  rounded-2xl p-3">
-                    <h2 className="text-xl font-bold text-sky-900 mb-4">Edit Task</h2>
-                    <form
-                         onSubmit={handleSubmite}
-                        className="flex flex-col gap-3 p-4   "
-                    >
 
-                        <input
-                            type="text"
-                            placeholder="Task Name"
-                            className="p-2 rounded border border-gray-300"
-                            value={title}
-                            onChange={(e) => setTitle(e.target.value)}
-                        />
-                        <select
-                            className="p-2 rounded border border-gray-300"
-                            value={status}
-                            onChange={(e)=> setStatus(e.target.value)}
-                        >
-                            <option value="Low">Low</option>
-                            <option value="Medium">Medium</option>
-                            <option value="High">High</option>
-                        </select>
-                        <button  className="bg-sky-600 hover:bg-sky-700 text-white p-2 rounded "
-                        type="submit"
-                        >
-                            Update
-                        </button>
-                        <BaseButton Text="cancle" 
-                        onClick={onClose}
-                        />
-                        
-                    </form>
-                </div>
-            </div>
-        </>
-    )
+          <div className="flex justify-between mt-4  gap-3">
+
+          <button type="submit"
+              className="bg-sky-700 hover:bg-sky-700 text-white p-2 rounded w-[9rem]">
+              Save
+            </button>
+            <button type="button" onClick={onClose}
+              className="bg-red-700 text-white px-3 py-1 w-[9rem] rounded">
+              Cancel
+            </button>
+
+           
+          </div>
+
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default EditModal;
