b820b0a3d5349a7322efe439c94cfa4e97c23cf9
phatemeh amiri <fatemehamiri978@gmail.com>
2026-02-27 01:46:36 +0330

    configure pwa plugin

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,31 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    host: true,
-    port: 5173,
-  },   
-});
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: { enabled: true }, // برای تست در dev
+
+      manifest: {
+        name: 'toDoList App',
+        short_name: 'toDoList',
+        description: 'Modern to list to take care your  Task:) built with React & Tailwind',
+        theme_color: '#0ea5e9',
+        background_color: '#0ea5e9',
+        display: 'standalone',
+        start_url: '/',
+        icons: [
+          {
+            src: '/7.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ]
+})
                                                                                                                                                                                                                                                                                                                                               
