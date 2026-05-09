3ad0b3a232b0304c2da303036b90012f2d08cc34
phatemeh amiri <fatemehamiri978@gmail.com>
2026-05-09 18:21:07 +0330

    edit UI

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -67,7 +67,7 @@ if (!self.define) {
     });
   };
 }
-define(['./workbox-6fc00345'], (function (workbox) { 'use strict';
+define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
 
   self.skipWaiting();
   workbox.clientsClaim();
@@ -82,20 +82,11 @@ define(['./workbox-6fc00345'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.c76m6ptmj5o"
+    "revision": "0.1kuoskbch4o"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
     allowlist: [/^\/$/]
   }));
-  workbox.registerRoute(/^https:\/\/api\.ToDoList\.com\/.*/i, new workbox.NetworkFirst({
-    "cacheName": "ToDoList-cache",
-    plugins: [new workbox.ExpirationPlugin({
-      maxEntries: 10,
-      maxAgeSeconds: 3600
-    }), new workbox.CacheableResponsePlugin({
-      statuses: [0, 200]
-    })]
-  }), 'GET');
 
 }));
--- src/component/searchBar/searchBar.jsx
+++ src/component/searchBar/searchBar.jsx
@@ -6,7 +6,7 @@ function SearchBar({ Search, setSearch }) {
       <>
          <div className="w-[100%]   h-[7rem]  md:h-[4rem]  p-4 flex   flex-col  md:flex-row gap-2   ">
            
-            <div className=" h-[4rem]  md:w-[20%]  lg:w-[15%]  xl:w-[11%] rounded-xl flex gap-2 shadow-md  md:p-2  p-3   bg-sky-100">
+            <div className=" h-[4rem]  md:w-[20%]   rounded-xl flex gap-2 shadow-md  md:p-2  p-3  flex justify-between bg-sky-100">
                <div className="flex   flex-col font-bold   ">
                   <span className="text-sky-900">Fatemeh</span>
                   <span className="text-red-800"  >Amiri</span>
@@ -15,7 +15,7 @@ function SearchBar({ Search, setSearch }) {
             </div>
             <input type="text"
                placeholder="Search Task"
-               className="w-[100%] md:w-[80%] lg:w-[85%]  xl:w-[89%] h-[2rem]   bg-gray-200   p-7 
+               className="w-[100%] md:w-[80%]   h-[2rem]   bg-gray-200   p-7 
                rounded-full border-0  shadow-md placeholder-gray-600  focus:outline-none"
                value={Search}
                onChange={(e) => setSearch(e.target.value)}
